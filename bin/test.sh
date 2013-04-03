1bb10f0833006d6c6897d86a6b98b85fb71eb927
rnml <none@none>
2013-04-03 01:01:09 -0400

    formatting output

--- bin/test.sh
+++ bin/test.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-rm -rf parser.ml lexer.ml
 make parser.ml
 make lexer.ml
 rm .depend
 make
+echo ------------------------------------------------------------
 ./linear.exe <<EOF
   2*a + 17*b - 27*c = 1;
   5*a + 81*b -  6*c =
 -21*a + 47*b + 51*c = 0;
 EOF
+echo ------------------------------------------------------------
