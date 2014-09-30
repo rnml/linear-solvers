32838de950b97573e28acf99b7fe9859b619d15d
rnml <none@none>
2014-09-29 22:14:48 -0400

    cram-based test

--- bin/test.sh
+++ bin/test.sh
@@ -1,12 +1,3 @@
 #!/bin/bash
-make parser.ml
-make lexer.ml
-rm .depend
-make
-echo ------------------------------------------------------------
-./linear.exe <<EOF
-  2*a + 17*b - 27*c = 1;
-  5*a + 81*b -  6*c =
--21*a + 47*b + 51*c = 0;
-EOF
-echo ------------------------------------------------------------
+cd $(dirname $(readlink -f $0))
+TMPDIR=$(pwd) cram ./test.t
